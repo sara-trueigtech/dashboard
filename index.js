4bf0d823d2d62408ec7ca6f5ebcb41bf9ae90a50
sara-trueigtech <sakhan@trueigtech.com>
2026-01-19 14:29:03 +0530

    feat: display users and delete functionality added

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(json => console.log(json))
+import {api} from "./api";
+import {renderUser} from "./ui";
+
+async function loadUsers() {
+    const users = await api.getUsers();
+    renderUser(users);
+}
+
+loadUsers();
+
+document.addEventListener("click", async (e) => {
+    if(e.target.tagName === "BUTTON"){
+        const id = e.target.dataset.id;
+        await api.deleteUser(id);
+        loadUsers();
+    }
+});
